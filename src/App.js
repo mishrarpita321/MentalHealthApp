155ae4fd959c9ff4716b090d2027a56d86407b65
Shivani <shivanishrungare@gmail.com>
2023-07-12 13:30:12 +0200

    diary feature and routes added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Auth } from './screens/auth/Auth';
 import LandingPageDesktop from './components/NavigationBar/DesktopDevice/LandingPageDesktop';
+import LandingPage from './components/NavigationBar/DesktopDevice/LandingPage';
 import { Routes, Route, Link } from "react-router-dom";
 import Diary from './components/Diary/Diary';
 import AssessmentQns from './components/AssessmentQns/AssessmentQns';
@@ -11,10 +12,12 @@ function App() {
     <div>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPageDesktop />}/>
+        <Route path="/" element={<LandingPage/>}/>
         <Route path="/login" element={<Auth/>}/>
-        <Route path='/diary' element={<Diary/>}/>
+        <Route path="/userlogin" exact element={<LandingPageDesktop/>}/>
+        <Route path='/userlogin/diary' element={<Diary/>}/>
         <Route path='/assessment' element={<AssessmentQns/>}/>
+        <Route path='/diary' element={<Diary/>}/>
       </Routes>
     </BrowserRouter>
     </div>
